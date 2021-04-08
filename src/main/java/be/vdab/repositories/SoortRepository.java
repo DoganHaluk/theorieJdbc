ab982162b99aee410aa9b5e63c06b146b134e9b4
DoganHaluk <ha_look@yahoo.com>
2021-04-08 06:02:19 +0200

    isolation level read_committed

--- src/main/java/be/vdab/repositories/SoortRepository.java
+++ src/main/java/be/vdab/repositories/SoortRepository.java
@@ -8,20 +8,24 @@ import java.sql.SQLException;
 public class SoortRepository extends AbstractRepository {
     public void create(String naam) throws SQLException {
         try (var connection = super.getConnection();
-             var statementSelect = connection.prepareStatement(
-                     "select id from soorten where naam = ?")) {
-            statementSelect.setString(1, naam);
-            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+             var statementInsert = connection.prepareStatement(
+                     "insert into soorten(naam) values (?)")) {
+            statementInsert.setString(1, naam);
+            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             connection.setAutoCommit(false);
-            if (statementSelect.executeQuery().next()) {
+            try {
+                statementInsert.executeUpdate();
                 connection.commit();
-                throw new SoortBestaatAlException();
-            } else {
-                try (var statementInsert = connection.prepareStatement(
-                        "insert into soorten(naam) values (?)")) {
-                    statementInsert.setString(1, naam);
-                    statementInsert.executeUpdate();
+            } catch (SQLException ex) {
+                try (var statementSelect = connection.prepareStatement(
+                        "select id from soorten where naam = ?")) {
+                    statementSelect.setString(1, naam);
+                    if (statementSelect.executeQuery().next()) {
+                        connection.commit();
+                        throw new SoortBestaatAlException();
+                    }
                     connection.commit();
+                    throw ex;
                 }
             }
         }
